4ba81973bf296e5a30f5237c4a1266c80ac6f491
Samuel-Brr <samuel.barre972@gmail.com>
2022-04-01 15:24:10 +0200

    Création du auth store et user model ainsi que de la auth-guard

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -3,9 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 import { ConnexionComponent } from './component/connexion/connexion.component';
 import { DashboardComponent } from './component/dashboard/dashboard.component';
 import { InscriptionComponent } from './component/inscription/inscription.component';
+import { IsAuthGuard } from './is-auth.guard';
 
 const routes: Routes = [
-  {path:"", component:DashboardComponent},
+  {path:"", component:DashboardComponent,
+    canActivate:[IsAuthGuard]},
   {path:"inscription", component:InscriptionComponent},
   {path:"connexion", component:ConnexionComponent}
 ];
